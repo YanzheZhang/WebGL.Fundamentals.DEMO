3d99147e314d0d3cc0588a23f66ad1139ea5067f
YanzheZhang <gishpuzyz@163.com>
2019-08-28 20:22:11 +0800

    1完善WebGL 基础概念 2添加WebGL 工作原理
    Signed-off-by: YanzheZhang <gishpuzyz@163.com>

---
+++ lesson/01Fundamentals/01Fundamentals-03.js
@@ -0,0 +1,153 @@
+﻿/* eslint no-console:0 consistent-return:0 */
+"use strict";
+
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+function main() {
+    // Get A WebGL context
+    var canvas = document.getElementById("c");
+    var gl = canvas.getContext("webgl");
+    if (!gl) {
+        return;
+    }
+
+    // Get the strings for our GLSL shaders
+    var vertexShaderSource = document.getElementById("2d-vertex-shader").text;
+    var fragmentShaderSource = document.getElementById("2d-fragment-shader").text;
+
+    // create GLSL shaders, upload the GLSL source, compile the shaders
+    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    // Link the two shaders into a program
+    var program = createProgram(gl, vertexShader, fragmentShader);
+
+    // look up where the vertex data needs to go.
+    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+    var colorUniformLocation = gl.getUniformLocation(program, "u_color");
+
+    // Create a buffer and put three 2d clip space points in it
+    var positionBuffer = gl.createBuffer();
+
+    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    var positions = [
+       10, 20,
+       80, 20,
+       10, 30,
+       10, 30,
+       80, 20,
+       80, 30,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    // code above this line is initialization code.
+    // code below this line is rendering code.
+
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // Clear the canvas
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
+
+    // Turn on the attribute
+    gl.enableVertexAttribArray(positionAttributeLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        positionAttributeLocation, size, type, normalize, stride, offset);
+
+    // 设置全局变量 分辨率
+    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+    // draw
+    //var primitiveType = gl.TRIANGLES;
+    //var offset = 0;
+    //var count = 6;
+    //gl.drawArrays(primitiveType, offset, count);
+    // 绘制50个随机颜色矩形
+    for (var ii = 0; ii < 50; ++ii) {
+        // 创建一个随机矩形
+        // 并将写入位置缓冲
+        // 因为位置缓冲是我们绑定在
+        // `ARRAY_BUFFER`绑定点上的最后一个缓冲
+        setRectangle(
+            gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
+
+        // 设置一个随机颜色
+        gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+
+        // 绘制矩形
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+    }
+
+    // 返回 0 到 range 范围内的随机整数
+    function randomInt(range) {
+        return Math.floor(Math.random() * range);
+    }
+
+    // 用参数生成矩形顶点并写进缓冲
+
+    function setRectangle(gl, x, y, width, height) {
+        var x1 = x;
+        var x2 = x + width;
+        var y1 = y;
+        var y2 = y + height;
+
+        // 注意: gl.bufferData(gl.ARRAY_BUFFER, ...) 将会影响到
+        // 当前绑定点`ARRAY_BUFFER`的绑定缓冲
+        // 目前我们只有一个缓冲，如果我们有多个缓冲
+        // 我们需要先将所需缓冲绑定到`ARRAY_BUFFER`
+
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+           x1, y1,
+           x2, y1,
+           x1, y2,
+           x1, y2,
+           x2, y1,
+           x2, y2]), gl.STATIC_DRAW);
+    }
+}
+
+main();
---
+++ lesson/01Fundamentals/02HowItWorks-03.js
@@ -0,0 +1,171 @@
+﻿/* eslint no-console:0 consistent-return:0 */
+"use strict";
+
+function main() {
+    // Get A WebGL context
+    /** @type {HTMLCanvasElement} */
+    var canvas = document.getElementById("canvas");
+    var gl = canvas.getContext("webgl");
+    if (!gl) {
+        return;
+    }
+
+    // setup GLSL program
+    var program = webglUtils.createProgramFromScripts(gl, ["2d-vertex-shader", "2d-fragment-shader"]);
+
+    // look up where the vertex data needs to go.
+    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    var colorLocation = gl.getAttribLocation(program, "a_color");
+
+    // lookup uniforms
+    var matrixLocation = gl.getUniformLocation(program, "u_matrix");
+
+    // Create a buffer.
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // Set Geometry.
+    setGeometry(gl);
+
+    // Create a buffer for the colors.
+    var colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    // Set the colors.
+    setColors(gl);
+
+    var translation = [200, 150];
+    var angleInRadians = 0;
+    var scale = [1, 1];
+
+    drawScene();
+
+    // Setup a ui.
+    webglLessonsUI.setupSlider("#x", { value: translation[0], slide: updatePosition(0), max: gl.canvas.width });
+    webglLessonsUI.setupSlider("#y", { value: translation[1], slide: updatePosition(1), max: gl.canvas.height });
+    webglLessonsUI.setupSlider("#angle", { slide: updateAngle, max: 360 });
+    webglLessonsUI.setupSlider("#scaleX", { value: scale[0], slide: updateScale(0), min: -5, max: 5, step: 0.01, precision: 2 });
+    webglLessonsUI.setupSlider("#scaleY", { value: scale[1], slide: updateScale(1), min: -5, max: 5, step: 0.01, precision: 2 });
+
+    function updatePosition(index) {
+        return function (event, ui) {
+            translation[index] = ui.value;
+            drawScene();
+        };
+    }
+
+    function updateAngle(event, ui) {
+        var angleInDegrees = 360 - ui.value;
+        angleInRadians = angleInDegrees * Math.PI / 180;
+        drawScene();
+    }
+
+    function updateScale(index) {
+        return function (event, ui) {
+            scale[index] = ui.value;
+            drawScene();
+        };
+    }
+
+    // Draw the scene.
+    function drawScene() {
+        webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+        // Tell WebGL how to convert from clip space to pixels
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        // Clear the canvas.
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        // Tell it to use our program (pair of shaders)
+        gl.useProgram(program);
+
+        // Turn on the attribute
+        gl.enableVertexAttribArray(positionAttributeLocation);
+
+        // Bind the position buffer.
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+        // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+        var size = 2;          // 2 components per iteration
+        var type = gl.FLOAT;   // the data is 32bit floats
+        var normalize = false; // don't normalize the data
+        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;        // start at the beginning of the buffer
+        gl.vertexAttribPointer(
+            positionAttributeLocation, size, type, normalize, stride, offset);
+
+
+        // Turn on the color attribute
+        gl.enableVertexAttribArray(colorLocation);
+
+        // Bind the color buffer.
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+
+        // Tell the color attribute how to get data out of colorBuffer (ARRAY_BUFFER)
+        var size = 4;          // 4 components per iteration
+        var type = gl.UNSIGNED_BYTE;  // 数据类型是8位的 UNSIGNED_BYTE 类型。
+        var normalize = true; // don't normalize the data
+        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;        // start at the beginning of the buffer
+        gl.vertexAttribPointer(
+            colorLocation, size, type, normalize, stride, offset);
+
+
+        // Compute the matrix
+        var matrix = m3.projection(gl.canvas.clientWidth, gl.canvas.clientHeight);
+        matrix = m3.translate(matrix, translation[0], translation[1]);
+        matrix = m3.rotate(matrix, angleInRadians);
+        matrix = m3.scale(matrix, scale[0], scale[1]);
+
+        // Set the matrix.
+        gl.uniformMatrix3fv(matrixLocation, false, matrix);
+
+        // Draw the geometry.
+        var primitiveType = gl.TRIANGLES;
+        var offset = 0;
+        var count = 6;
+        gl.drawArrays(primitiveType, offset, count);
+    }
+}
+
+// Fill the buffer with the values that define a rectangle.
+// Note, will put the values in whatever buffer is currently
+// bound to the ARRAY_BUFFER bind point
+function setGeometry(gl) {
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            -150, -100,
+             150, -100,
+            -150, 100,
+             150, -100,
+            -150, 100,
+             150, 100]),
+        gl.STATIC_DRAW);
+}
+
+// Fill the buffer with colors for the 2 triangles
+// that make the rectangle.
+// Note, will put the values in whatever buffer is currently
+// bound to the ARRAY_BUFFER bind point
+function setColors(gl) {
+    // 设置两个随机颜色
+    var r1 = Math.random() * 256; // 0 到 255.99999 之间
+    var b1 = Math.random() * 256; // 这些数据
+    var g1 = Math.random() * 256; // 在存入缓冲时
+    var r2 = Math.random() * 256; // 将被截取成
+    var b2 = Math.random() * 256; // Uint8Array 类型
+    var g2 = Math.random() * 256;
+
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Uint8Array(   // Uint8Array
+          [r1, b1, g1, 255,
+            r1, b1, g1, 255,
+            r1, b1, g1, 255,
+            r2, b2, g2, 255,
+            r2, b2, g2, 255,
+            r2, b2, g2, 255]),
+        gl.STATIC_DRAW);
+}
+
+main();
